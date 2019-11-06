7b71652cb2aee8c64045eeaeeb9347b12d882471
James Lucktaylor <jlucktay+github@gmail.com>
2019-11-06 18:15:00 +0000

    Add helper script to re-roll the VM.

---
+++ roll-vm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mapfile -t old_instances < <( gcloud compute instances list --format=json | jq --raw-output '.[].name' )
+
+for old_instance in "${old_instances[@]}"; do
+    echo "old instance: $old_instance"
+    gcloud compute instances delete "$old_instance" --format=json --quiet
+done
+
+new_instance=$( gcloud compute instances create "factorio-$( gdate '+%Y%m%d-%H%M%S' )" \
+    --format=json \
+    --maintenance-policy=TERMINATE \
+    --metadata-from-file startup-script=startup.sh,shutdown-script=shutdown.sh \
+    --preemptible \
+    --source-instance-template=factorio-container-10 \
+    --tags=factorio,grafana,ssh )
+
+new_instance_name=$( echo "$new_instance" | jq --raw-output'.[].name' )
+
+echo "new instance name: '$new_instance_name'"
+
+gcloud compute instances tail-serial-port-output "$new_instance_name" | grep startup-script
