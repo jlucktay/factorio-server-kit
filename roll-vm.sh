1dfe79230b42d50ffce007fe5b20e296c6e92d23
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-06 11:22:32 +0000

    Echo the IP of new servers.

--- roll-vm.sh
+++ roll-vm.sh
@@ -16,9 +16,11 @@ new_instance=$(gcloud compute instances create "factorio-$(gdate '+%Y%m%d-%H%M%S
   --source-instance-template=factorio-container-22)
 
 new_instance_id=$(echo "$new_instance" | jq --raw-output '.[].id')
+new_instance_ip=$(echo "$new_instance" | jq --raw-output '.[].networkInterfaces[].accessConfigs[].natIP')
 
-logs_link="https://console.cloud.google.com/logs/viewer?project=jlucktay-factorio&resource=gce_instance/instance_id/${new_instance_id}&interval=NO_LIMIT"
+echo "Server IP: $new_instance_ip"
+
+logs_link="https://console.cloud.google.com/logs/viewer?project=jlucktay-factorio&resource=gce_instance/instance_id/${new_instance_id}"
 
 echo "Opening the log viewer link: '$logs_link'"
-
 open "$logs_link"
