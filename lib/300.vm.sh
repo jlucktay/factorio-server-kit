43740e9b2d2b0eed7a87026273fc4aa6f992d779
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-19 21:50:57 +1100

    Fix up local declarations.

--- lib/300.vm.sh
+++ lib/300.vm.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 function factorio::vm::delete_all() {
-  local old_instances, i
+  local old_instances i
   old_instances=$(
     gcloud compute instances list \
       --format=json \
@@ -10,7 +10,7 @@ function factorio::vm::delete_all() {
   )
 
   for ((i = 0; i < $(echo "$old_instances" | jq length); i += 1)); do
-    local name, raw_zone, zone
+    local name zone
     name=$(jq --raw-output ".[$i].name" <<< "$old_instances")
     zone=$(basename "$(jq --raw-output ".[$i].zone" <<< "$old_instances")")
 
