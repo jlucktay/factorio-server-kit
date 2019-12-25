bd6267b3bdfa803a5d81261704cc8364198eea13
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-26 07:36:05 +1100

    Preserve timestamps with this sync script also.

--- scripts/gsutil.sync.sh
+++ scripts/gsutil.sync.sh
@@ -4,7 +4,7 @@ shopt -s nullglob globstar
 
 mkdir -pv gs/jlucktay-factorio-asia/
 
-gsutil -m rsync -r -u \
+gsutil -m rsync -P -r -u \
   -x "saves/_autosave.\.zip$|saves/_autosave.\.tmp\.zip$|saves-.*/.*\.zip$" \
   gs://jlucktay-factorio-asia/ \
   ./gs/jlucktay-factorio-asia/
