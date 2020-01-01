ef4d2d5185eb7d891ed1bfc065e3ce22553273ec
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-02 08:30:13 +1100

    Update shutdown script to also pushes saves locally.

--- cloud-build/1-factorio-server/shutdown.sh
+++ cloud-build/1-factorio-server/shutdown.sh
@@ -3,13 +3,28 @@ set -euxo pipefail
 IFS=$'\n\t'
 
 logger "=== Called 'shutdown-script'!"
+locations_json="/etc/locations.json"
 
 logger "=== Waiting for Factorio container to stop..."
 while docker top factorio &> /dev/null; do
   sleep 1s
 done
 
+logger "=== Get instance's zone from metadata, to back up saves to the local bucket"
+instance_zone=$(
+  curl \
+    --header "Metadata-Flavor: Google" \
+    --silent \
+    metadata.google.internal/computeMetadata/v1/instance/zone
+)
+
+push_saves_to=$(
+  jq --raw-output \
+    '.[] | select(.zone == "'"$(basename "$instance_zone")"'") | .location' \
+    "$locations_json"
+)
+
 logger "=== Pushing Factorio saves to Storage..."
-gsutil -m rsync -P -x ".*\.tmp\.zip" /opt/factorio/saves gs://jlucktay-factorio-asia/saves |& logger
+gsutil -m rsync -P -x ".*\.tmp\.zip" /opt/factorio/saves "gs://jlucktay-factorio-saves-$push_saves_to" |& logger
 
 logger "=== Done!"
