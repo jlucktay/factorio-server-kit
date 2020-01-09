852152dd3fd8c111dcccd9053050246bc8415e0e
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-10 07:34:08 +1100

    Iterate through location-specific '-saves' buckets.

--- scripts/gsutil-backup-saves.sh
+++ scripts/gsutil-backup-saves.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 shopt -s nullglob globstar
+IFS=$'\n\t'
 
-gsutil -m \
-  rsync -P -x ".*\.tmp\.zip" \
-  gs://jlucktay-factorio-asia/saves \
-  "gs://jlucktay-factorio-asia/saves-$(TZ=UTC gdate +%Y%m%d)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
+FACTORIO_ROOT=$script_dir/..
+
+for lib in "${FACTORIO_ROOT}"/lib/*.sh; do
+  # shellcheck disable=SC1090
+  source "$lib"
+done
+
+# Iterate through location-specific '-saves' buckets, and backup into another bucket with timestamp sub-directories
+
+# shellcheck disable=SC2154
+for location in "${!FACTORIO_SERVER_LOCATIONS[@]}"; do
+  stat=$(gsutil stat "gs://jlucktay-factorio-saves-$location/_autosave*.zip" 2> /dev/null || true)
+
+  if [ ${#stat} -eq 0 ]; then
+    continue
+  fi
+
+  mtimes=$(grep goog-reserved-file-mtime <<< "$stat" | cut -d":" -f2)
+  mtime_high_score=0
+
+  for mtime in $mtimes; do
+    if ((mtime > mtime_high_score)); then
+      mtime_high_score=$mtime
+    fi
+  done
+
+  gsutil -m \
+    rsync -P -x ".*\.tmp\.zip" \
+    "gs://jlucktay-factorio-saves-$location" \
+    "gs://jlucktay-factorio-asia/saves-$location-$(TZ=UTC date -r "$mtime_high_score" +%Y%m%d)"
+done
