d1a9136a36e85895c93dbdb5203c7d6d93824636
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-19 11:17:18 +1100

    Start breaking down Grafana commands - one time vs per boot.

--- scripts/grafana-setup.sh
+++ scripts/grafana-setup.sh
@@ -26,49 +26,49 @@ curl \
   "http://admin:admin@${FACTORIO_DNS_NAME:-}:3000/api/user/password" \
   | jq
 
+curl \
+  --data '{
+    "access": "proxy",
+    "isDefault": true,
+    "jsonData": {
+      "timeInterval": "10s"
+    },
+    "name": "Graftorio - Prometheus",
+    "type": "prometheus",
+    "url": "http://prometheus:9090"
+  }' \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --silent \
+  "http://admin:${FACTORIO_PASSWORD}@$FACTORIO_DNS_NAME:3000/api/datasources" \
+  | jq
+
 # Per boot
 
 # One time vs per boot? TBD
 
-# curl \
-#     --data '{
-#         "access": "proxy",
-#         "isDefault": true,
-#         "jsonData": {
-#             "timeInterval": "10s"
-#         },
-#         "name": "Graftorio - Prometheus",
-#         "type": "prometheus",
-#         "url": "http://prometheus:9090"
-#     }' \
-#     --header "Accept: application/json" \
-#     --header "Content-Type: application/json" \
-#     --request POST \
-#     --silent \
-#     "http://admin:${FACTORIO_PASSWORD}@${factorio_instance_ip}:3000/api/datasources" \
-#     | jq
-
-# curl \
-#     --data '{
-#         "dashboard": {
-#             "id": null,
-#             "timezone": "browser",
-#             "title": "Hello Factorio",
-#             "uid": null
-#         },
-#         "message": "grafana-setup.sh",
-#         "overwrite": false
-#     }' \
-#     --header "Accept: application/json" \
-#     --header "Content-Type: application/json" \
-#     --request POST \
-#     --silent \
-#     "http://admin:${password}@${factorio_instance_ip}:3000/api/dashboards/db" \
-#     | jq
+curl \
+  --data '{
+    "dashboard": {
+      "id": null,
+      "timezone": "browser",
+      "title": "Hello Factorio",
+      "uid": null
+    },
+    "message": "grafana-setup.sh",
+    "overwrite": false
+  }' \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --silent \
+  "http://admin:${FACTORIO_PASSWORD}@${FACTORIO_DNS_NAME}:3000/api/dashboards/db" \
+  | jq
 
 curl \
   --header "Accept: application/json" \
   --request GET \
   --silent \
-  "http://admin:${FACTORIO_PASSWORD:-}@${factorio_instance_ip}:3000/api/dashboards/uid/Yv5ie31Wk" \
+  "http://admin:${FACTORIO_PASSWORD}@${FACTORIO_DNS_NAME}:3000/api/dashboards/uid/Yv5ie31Wk" \
   | jq
