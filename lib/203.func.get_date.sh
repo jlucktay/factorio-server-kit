e649191357c8237f5d41b27eb33e6de362a5ec8f
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-28 09:17:32 +0000

    Don't need output, just exit status.

--- lib/203.func.get_date.sh
+++ lib/203.func.get_date.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Necessary wrapper for 'date' calls other than (for example) 'date "+%Y%m%d.%H%M%S%z"'
 function factorio::run_date() {
-  if hash gdate; then
+  if hash gdate &> /dev/null; then
     gdate "$@"
   else
     date "$@"
