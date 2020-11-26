9599a5fce56980ab512f8ca2576091d9ce0e39ec
James Lucktaylor <jlucktay+github@gmail.com>
2020-11-26 09:09:02 +0000

    fix(factorio): use 'stable' rather than 'latest' image tag

--- cloud-build/1-factorio-server/docker-run-factorio.sh
+++ cloud-build/1-factorio-server/docker-run-factorio.sh
@@ -2,17 +2,22 @@
 set -euxo pipefail
 IFS=$'\n\t'
 
-docker rm factorio || true
-docker pull factoriotools/factorio:latest
+container_name="factorio"
+image_name="factoriotools/factorio"
+image_tag="stable"
+image="$image_name":"$image_tag"
+
+docker rm "$container_name" || true
+docker pull "$image"
 
 docker run \
   --detach \
   --env USERNAME \
   --env TOKEN \
   --env UPDATE_MODS_ON_START \
-  --name=factorio \
+  --name="$container_name" \
   --publish=27015:27015/tcp \
   --publish=34197:34197/udp \
   --restart=on-failure \
-  --volume=/opt/factorio:/factorio \
-  factoriotools/factorio:latest
+  --volume=/opt/"$container_name":/"$container_name" \
+  "$image"
