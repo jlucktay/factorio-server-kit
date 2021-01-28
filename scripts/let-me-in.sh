870073a9b91b72a704f77dd2efd2fc871621910b
James Lucktaylor <jlucktay+github@gmail.com>
2021-01-28 19:19:55 +0000

    feat(scripts/let-me-in): update firewall rule only if it needs it

--- scripts/let-me-in.sh
+++ scripts/let-me-in.sh
@@ -12,32 +12,43 @@ curl_output=$(curl --silent http://httpbin.org/ip | jq --raw-output '.origin')
 [[ $curl_output =~ ([0-9]{1,3}\.){3}[0-9]{1,3} ]]
 my_ip=${BASH_REMATCH[0]}
 
-### Build arguments list for gcloud
-gcloud_firewall_update_args=(
+### Look at current rule to see if it needs to be changed
+gcloud_firewall_describe_args=(
   compute
   firewall-rules
-  update
-  "--source-ranges=$my_ip/32"
+  describe
   default-allow-ssh
+  --format=json
 )
 
-### Show arguments and execute with them
-echo -n "Updating firewall rules: gcloud "
-echo "${gcloud_firewall_update_args[@]}"
-gcloud "${gcloud_firewall_update_args[@]}"
+echo "Describing firewall rule: gcloud ${gcloud_firewall_describe_args[*]}"
+description=$(gcloud "${gcloud_firewall_describe_args[@]}")
+
+### Update the firewall rule if the IP differs
+if [ "$my_ip/32" != "$(jq --raw-output '.sourceRanges[0]' <<< "$description")" ]; then
+  gcloud_firewall_update_args=(
+    compute
+    firewall-rules
+    update
+    default-allow-ssh
+    "--source-ranges=$my_ip/32"
+  )
+
+  echo "Updating firewall rule: gcloud ${gcloud_firewall_update_args[*]}"
+  gcloud "${gcloud_firewall_update_args[@]}"
+fi
 
 ### Get Factorio server instance and SSH into it
 gcloud_instance_list_args=(
-  --format json
   compute
   instances
   list
-  --filter "name:factorio-*"
-  --limit 1
+  --filter="name:factorio-*"
+  --format=json
+  --limit=1
 )
 
-echo -n "Listing existing instances: gcloud "
-echo "${gcloud_instance_list_args[@]}"
+echo "Listing existing instances: gcloud ${gcloud_instance_list_args[*]}"
 instance=$(gcloud "${gcloud_instance_list_args[@]}")
 
 if [ "$(jq length <<< "$instance")" == 0 ]; then
@@ -50,10 +61,9 @@ zone=$(basename "$(jq --raw-output ".[0].zone" <<< "$instance")")
 gcloud_ssh_args=(
   compute
   ssh
-  --zone "$zone"
   "$name"
+  --zone="$zone"
 )
 
-echo -n "SSHing into Factorio server instance: gcloud "
-echo "${gcloud_ssh_args[@]}"
+echo "SSHing into Factorio server instance: gcloud ${gcloud_ssh_args[*]}"
 gcloud "${gcloud_ssh_args[@]}"
