5a0b93d9eeb205ba86fffb35dede9a51203400b5
James Lucktaylor <jlucktay+github@gmail.com>
2021-01-28 20:43:32 +0000

    refactor(lib): clean up DNS function

--- lib/204.func.dns.update.sh
+++ lib/204.func.dns.update.sh
@@ -6,20 +6,16 @@ set -euo pipefail
 # - 2: IP to update into the record
 
 function factorio::dns::update() {
-  set -x
-
   # The DNS name/'A' record to update ["factorio.menagerie.games.", "minecraft.menagerie.games."]
   local dns_name="${1:?}.menagerie.games."
+  local zone="$1-server"
 
-  echo "Updating the 'A' record '$dns_name' in Cloud DNS with new IP '${2:?}'..."
-
-  local zone="factorio-server" # TODO: straighten this out, and stop running DNS out of one project
+  echo "Updating the 'A' record '$dns_name' in zone '$zone' of Cloud DNS with new IP '${2:?}'..."
 
   gcloud \
     dns record-sets transaction \
     start \
-    --zone="$zone" \
-    &> /dev/null
+    --zone="$zone"
 
   old_dns_ip=$(
     gcloud --format=json \
@@ -35,8 +31,7 @@ function factorio::dns::update() {
     --name="$dns_name" \
     --ttl=30 \
     --type=A \
-    --zone="$zone" \
-    &> /dev/null
+    --zone="$zone"
 
   gcloud \
     dns record-sets transaction \
@@ -44,14 +39,10 @@ function factorio::dns::update() {
     --name="$dns_name" \
     --ttl=30 \
     --type=A \
-    --zone="$zone" \
-    &> /dev/null
+    --zone="$zone"
 
   gcloud \
     dns record-sets transaction \
     execute \
-    --zone="$zone" \
-    &> /dev/null
-
-  set +x
+    --zone="$zone"
 }
