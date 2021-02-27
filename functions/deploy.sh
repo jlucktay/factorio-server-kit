855defa887699ea8700b010dc6ee214c415240c1
James Lucktaylor <jlucktay+github@gmail.com>
2021-02-27 12:01:29 +0000

    ci(functions): deploy all dynamically discovered funcs

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,59 +1,65 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly FACTORIO_ROOT="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && git rev-parse --show-toplevel)"
+readonly script_dir=$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)
+readonly FACTORIO_ROOT="$(git -C "$script_dir" rev-parse --show-toplevel)"
 
 for lib in "$FACTORIO_ROOT"/lib/*.sh; do
   # shellcheck disable=SC1090
   source "$lib"
 done
 
-function_name="cleanup-instances"
-topic_name=$function_name
+cd "$script_dir"
 
-gcloud_list_args=(
-  --format json
-  functions
-  list
-  --filter "name:$function_name"
-)
+mapfile -t func_names < <(go doc | awk -F' |\\\(' '$1 == "func" { print $2 }')
 
-echo "Looking for existing '$function_name' functions deployed outside the '${CLOUDSDK_FUNCTIONS_REGION:?}' region..."
-mapfile -t functions_list < <(gcloud "${gcloud_list_args[@]}" | jq --raw-output '.[].name')
+for func_name in ${func_names[*]}; do
+  function_name=$(echo "cleanup-$func_name" | awk '{ print tolower($0) }')
 
-delete_region=()
-
-for ((i = 0; i < ${#functions_list[@]}; i += 1)); do
-  func=${functions_list[$i]##*locations/}
-  func=${func%%/functions*}
-
-  if [ "$func" != "${CLOUDSDK_FUNCTIONS_REGION:?}" ]; then
-    delete_region+=("$func")
-  fi
-done
-
-for ((i = 0; i < ${#delete_region[@]}; i += 1)); do
-  gcloud_delete_args=(
+  gcloud_list_args=(
+    --format json
     functions
-    delete
-    "$function_name"
-    --region "${delete_region[$i]}"
-    --quiet
+    list
+    --filter "name:$function_name"
   )
 
-  echo "Cleaning up existing function: gcloud ${gcloud_delete_args[*]}"
-  gcloud "${gcloud_delete_args[@]}"
+  echo "Looking for existing '$function_name' functions deployed outside the '${CLOUDSDK_FUNCTIONS_REGION:?}' region..."
+  mapfile -t functions_list < <(gcloud "${gcloud_list_args[@]}" | jq --raw-output '.[].name')
+
+  delete_region=()
+
+  for ((i = 0; i < ${#functions_list[@]}; i += 1)); do
+    func=${functions_list[$i]##*locations/}
+    func=${func%%/functions*}
+
+    if [ "$func" != "${CLOUDSDK_FUNCTIONS_REGION:?}" ]; then
+      delete_region+=("$func")
+    fi
+  done
+
+  for ((i = 0; i < ${#delete_region[@]}; i += 1)); do
+    gcloud_delete_args=(
+      functions
+      delete
+      "$function_name"
+      --region "${delete_region[$i]}"
+      --quiet
+    )
+
+    echo "Cleaning up existing function: gcloud ${gcloud_delete_args[*]}"
+    gcloud "${gcloud_delete_args[@]}"
+  done
+
+  gcloud_deploy_args=(
+    functions
+    deploy
+    "$function_name"
+    --entry-point "$func_name"
+    --max-instances 1
+    --runtime go113
+    --trigger-topic "$function_name"
+  )
+
+  echo "Deploying '$function_name': gcloud ${gcloud_deploy_args[*]}"
+  gcloud "${gcloud_deploy_args[@]}"
 done
-
-gcloud_deploy_args=(
-  functions
-  deploy
-  "$function_name"
-  --entry-point "Instances"
-  --max-instances 1
-  --runtime go113
-  --trigger-topic "$topic_name"
-)
-
-echo "Deploying '$function_name': gcloud ${gcloud_deploy_args[*]}"
-gcloud "${gcloud_deploy_args[@]}"
