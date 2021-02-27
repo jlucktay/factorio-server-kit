93452cc2e0e934efafd12d7f79bc1de1851c3772
James Lucktaylor <jlucktay+github@gmail.com>
2021-02-27 12:02:59 +0000

    refactor(terraform): clean up helper scripts

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly FACTORIO_ROOT="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && git rev-parse --show-toplevel)"
+readonly script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
+readonly FACTORIO_ROOT="$(git -C "$script_dir" rev-parse --show-toplevel)"
 
 for lib in "$FACTORIO_ROOT"/lib/*.sh; do
   # shellcheck disable=SC1090
   source "$lib"
 done
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
 
 # shellcheck disable=SC2154
 export TF_VAR_project_id="$CLOUDSDK_CORE_PROJECT"
--- terraform/init.sh
+++ terraform/init.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-readonly FACTORIO_ROOT="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && git rev-parse --show-toplevel)"
+readonly script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
+readonly FACTORIO_ROOT="$(git -C "$script_dir" rev-parse --show-toplevel)"
 
 for lib in "$FACTORIO_ROOT"/lib/*.sh; do
   # shellcheck disable=SC1090
   source "$lib"
 done
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
 
 bucket_suffix="-tfstate"
 bucket_name="gs://${CLOUDSDK_CORE_PROJECT:?}$bucket_suffix"
