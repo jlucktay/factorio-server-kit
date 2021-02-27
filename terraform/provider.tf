30fa9aa58cc432108c3ca41baabc93285fa4753d
James Lucktaylor <jlucktay+github@gmail.com>
2021-02-27 12:02:37 +0000

    feat(terraform): upgrade to Terraform v0.14

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,3 @@
 provider "google" {
   project = var.project_id
-  version = "~> 3.5"
 }
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,12 @@
 terraform {
   backend "gcs" {}
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.58.0"
+    }
+  }
+
+  required_version = ">= 0.13"
 }
